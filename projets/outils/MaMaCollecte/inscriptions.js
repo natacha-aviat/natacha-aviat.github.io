5b5acd5272cf673aa8fe8dd70496d04bf4b2d93d
Natacha Aviat <31728582+natacha-aviat@users.noreply.github.com>
2026-01-23 18:28:28 -0500

    Mise à jour des inscriptions (3 inscription(s))

--- projets/outils/MaMaCollecte/inscriptions.js
+++ projets/outils/MaMaCollecte/inscriptions.js
@@ -1,5 +1,5 @@
 // Inscriptions pour MaMaCollecte
-// Généré le 23/01/2026 23:22:20
+// Généré le 23/01/2026 23:28:28
 const INSCRIPTIONS = [
     {
         id: "1769205690919",
@@ -30,5 +30,20 @@ const INSCRIPTIONS = [
         locationName: "Pantin",
         slotKey: "2024-01-15_slot1_lieu3",
         timestamp: "2026-01-23T23:22:20.021Z"
+    },
+    {
+        id: "1769210908104",
+        firstName: "Jb",
+        lastName: "Av",
+        phone: "0612345678",
+        email: "packs_hawker.2j@icloud.com",
+        dateKey: "2024-01-16",
+        dateStr: "mardi 16 janvier 2024",
+        slotId: "slot1",
+        slotTime: "10:00 - 13:00",
+        location: "lieu1",
+        locationName: "Aubervilliers",
+        slotKey: "2024-01-16_slot1_lieu1",
+        timestamp: "2026-01-23T23:28:28.104Z"
     }
 ];
